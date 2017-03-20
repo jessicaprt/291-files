ef00566cc49f1c41389ab1a6c14a906a4d77f013
Jessica Prieto <jessicaprieto31@gmail.com>
2017-03-20 01:23:03 -0600

    added description to methods and fixed commit to connection

--- cheatscript.py
+++ cheatscript.py
@@ -1,7 +1,15 @@
+""" 
+created by: Jessica Prieto
+a sample application
+i created this to take important notes on embedded SQL
+into a python application using cx_Oracle
+
+"""
 import cx_Oracle
 from pprint import pprint
 
 def oracle_Connect():
+    """ establishing a connection """
     auth = open('.info')
     username = auth.readline().strip()
     password = auth.readline().strip()
@@ -9,20 +17,28 @@ def oracle_Connect():
     print("connected")
     return connection
 
-def get_Cursor(con):    
+def get_Cursor(con):
+    """ getting the cursor from a connection """
     return con.cursor()
 
-def commit_And_Close(con):
-    con.commit
+def commit_And_Close(con, com):
+    """ commiting changes to DB and closing the connection """
+
+    if com == 1:
+        con.commit
+    
     con.close()
 
 def execute_List(statement, cursor):
+    """when query is executed, the result is saved in 
+        a list and then the list is printed"""
     print("printing list")
     cursor.execute(statement)
     slist = cursor.fetchall()
     pprint(slist)
 
 def execute_Fetchall(statement,cursor,bindvars=None):
+    """executing a query and printing by iterating through fetchall"""
     print("printing fetchall")
     
     if bindvars == None:
@@ -34,6 +50,7 @@ def execute_Fetchall(statement,cursor,bindvars=None):
         print(i)
 
 def execute_Fetchone(statement, cursor):
+    """executing a query and printing one by one"""
     print("printing by fetching one each")
     cursor.execute(statement)
     i = cursor.fetchone()
@@ -41,13 +58,23 @@ def execute_Fetchone(statement, cursor):
         print(i)
         i=cursor.fetchone()
 
-def search(rows, table, variable, cursor):
+def search(columns, table, variable, cursor):
+    """
+    searching for a value
+    arguments: 
+        columns - the desired columns to be returned by the query
+        table - the table to search from
+              - assumption: user is only searching from one table
+        variable - the column to be compared to
+        cursor - the current cursor
+    """
     string = input("search input: ")
     print("searching")
-    statement = "select {0} from {1} where upper({2}) = :1".format(rows, table, variable)
+    statement = "select {0} from {1} where upper({2}) = :1".format(columns, table, variable)
     execute_Fetchall(statement, cursor,(string.upper().ljust(15),)) 
 
 def get_Description(cursor):
+    """prints the description from the table that was last executed by the dursor"""
     rows = cursor.description
     for i in rows:
         print(i[0])
@@ -69,11 +96,10 @@ if __name__ == "__main__":
     execute_Fetchall(statement, cursor)
     execute_Fetchone(statement, cursor)
 
-    rows = "*"
+    cols = "*"
     table = "friends"
     variable = "name"
-    search(rows, table, variable, cursor)
+    search(cols, table, variable, cursor)
     
-    #to get and print the fow based on the last executed querys
     get_Description
-    commit_And_Close(connection)
+    commit_And_Close(connection, 1)
